1714d58c952edd432a8e4f20c86ae01d1914da30
m.parhizkar <mah.parhizkar88@gmail.com>
2022-11-13 16:17:20 +0330

    Added config to zuul for swagger

---
+++ zuul/src/main/java/com/egs/zuul/controller/DocumentationController.java
@@ -0,0 +1,39 @@
+package com.egs.zuul.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+import springfox.documentation.swagger.web.UiConfiguration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Primary
+@EnableAutoConfiguration
+public class DocumentationController implements SwaggerResourcesProvider {
+
+    @Override
+    public List get() {
+        List resources = new ArrayList<>();
+        resources.add(swaggerResource("test-service", "/test-service/v2/api-docs", "2.0"));
+        return resources;
+    }
+
+    private SwaggerResource swaggerResource(String name, String location, String version) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion(version);
+        return swaggerResource;
+    }
+
+    @Bean
+    UiConfiguration uiConfig() {
+        return new UiConfiguration("validatorUrl", "list", "alpha", "schema",
+                UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS, false, true, 60000L);
+    }
+}
